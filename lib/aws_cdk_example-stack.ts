2439338c74ae587d89218d98640c859fef4beb2c
Sidney Ng <yendisng@gmail.com>
2021-11-18 20:45:05 -0500

    add the service stack and create it in the constructor

--- lib/aws_cdk_example-stack.ts
+++ lib/aws_cdk_example-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from '@aws-cdk/core';
+import * as widget_service from '../lib/widget_service';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class AwsCdkExampleStack extends cdk.Stack {
@@ -6,6 +7,7 @@ export class AwsCdkExampleStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new widget_service.WidgetService(this, 'Widgets');
 
     // example resource
     // const queue = new sqs.Queue(this, 'AwsCdkExampleQueue', {
